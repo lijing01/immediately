97b5e89e6bf53db1cebf6e7d863e144523ccccb3
lijing01 <tracelijing@gmail.com>
2016-03-20 00:35:09 +0800

    okhttp demo
    Signed-off-by: lijing01 <tracelijing@gmail.com>

--- app/src/main/java/com/tracelijing/immediately/EntryActivityFragment.java
+++ app/src/main/java/com/tracelijing/immediately/EntryActivityFragment.java
@@ -1,15 +1,23 @@
 package com.tracelijing.immediately;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class EntryActivityFragment extends Fragment {
+	public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
 	public EntryActivityFragment() {
 	}
@@ -18,5 +26,35 @@ public class EntryActivityFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_entry, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		getData();
+	}
+
+	private void getData() {
+		 new Thread(){
+			public void run(){
+				try {
+					String url = "https://app.jike.ruguoapp.com/1.0/users/messages/list";
+					OkHttpClient client = new OkHttpClient();
+					RequestBody body = RequestBody.create(JSON, "{\"messageIdLessThan\":2147483647,\"limit\":25}");
+					Request request = new Request.Builder()
+							.url(url)
+							.post(body)
+							.build();
+
+					Response response = client.newCall(request).execute();
+					if(response!=null && response.body()!=null) {
+						Log.v("lijing", request.body().toString());
+					}
+				} catch (Exception exception) {
+					exception.printStackTrace();
+				}
+			}
+		}.start();
+
 	}
 }
