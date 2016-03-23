5f98e98ee11bc55350b6efb1db6d27d84f4af879
lijing01 <tracelijing@gmail.com>
2016-03-23 23:48:46 +0800

    header 工具类
    Signed-off-by: lijing01 <tracelijing@gmail.com>

--- app/src/main/java/com/tracelijing/immediately/net/OkCommonHeaderTool.java
+++ app/src/main/java/com/tracelijing/immediately/net/OkCommonHeaderTool.java
@@ -1,8 +1,29 @@
 package com.tracelijing.immediately.net;
 
+import android.content.Context;
+
+import com.tracelijing.tlibrary.AppUtils;
+import com.tracelijing.tlibrary.PhoneUtil;
+
+import java.util.HashMap;
+
 /**
  * Created by Trace (Tapatalk) on 2016/3/23.
  */
 public class OkCommonHeaderTool {
-
+	public static HashMap<String,String> hashMap;
+	public static HashMap<String,String> getCommentHeaders(Context context){
+		if(hashMap == null) {
+			hashMap = new HashMap<>();
+			hashMap.put("Manufacturer", PhoneUtil.getBrand());
+			hashMap.put("OS-Version", PhoneUtil.getVersion());
+			hashMap.put("Model", PhoneUtil.getModel());
+			hashMap.put("OS", "android");
+			hashMap.put("App-BuildNo", AppUtils.getAppVersionString(context));
+			hashMap.put("App-Version", String.valueOf(AppUtils.getAppVersion(context)));
+			hashMap.put("Host:", "app.jike.ruguoapp.com");
+			hashMap.put("Accept-Encoding", "gzip");
+		}
+		return hashMap;
+	}
 }
