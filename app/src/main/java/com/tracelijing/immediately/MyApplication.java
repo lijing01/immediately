2ebf9d053d528a8f130add4d84d433b6acb2d236
lijing01 <tracelijing@gmail.com>
2016-03-27 21:18:50 +0800

    集成 leakcanary
    Signed-off-by: lijing01 <tracelijing@gmail.com>

---
+++ app/src/main/java/com/tracelijing/immediately/MyApplication.java
@@ -0,0 +1,25 @@
+package com.tracelijing.immediately;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/**
+ * Created by Trace (Tapatalk) on 2016/3/27.
+ */
+public class MyApplication extends Application {
+	public static RefWatcher getRefWatcher(Context context) {
+		MyApplication application = (MyApplication) context.getApplicationContext();
+		return application.refWatcher;
+	}
+
+	private RefWatcher refWatcher;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		refWatcher = LeakCanary.install(this);
+	}
+}
---
+++ app/src/main/java/com/tracelijing/immediately/fragment/BaseFragment.java
@@ -0,0 +1,27 @@
+package com.tracelijing.immediately.fragment;
+
+import android.app.Fragment;
+import com.squareup.leakcanary.RefWatcher;
+import com.tracelijing.immediately.MyApplication;
+
+/**
+ * Created by Trace (Tapatalk) on 2016/3/27.
+ * BaseFragment contaion LeakCanary
+ */
+public class BaseFragment extends Fragment {
+	public void onResume() {
+		super.onResume();
+	}
+
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		RefWatcher refWatcher = MyApplication.getRefWatcher(getActivity());
+		refWatcher.watch(this);
+	}
+}
